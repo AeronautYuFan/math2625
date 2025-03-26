0ab5f08ee76555ceedb4aea6e2ddfcacef4a0617
AeronautYuFan <AeronautYuFan@gmail.com>
2025-03-26 00:15:10 -0400

    fixing proof real quick

---
+++ hw4/hw4cs2.R
@@ -0,0 +1,13 @@
+#case study 2
+library(DescTools)
+source('ORFun.R')
+
+# formatting stuff (add latex font)
+library(showtext)
+library(DescTools)
+library(Rfit)
+font_add(family = 'ComputerModern', regular = 'cmunrm.ttf') #for consistent formatting in LaTex
+showtext_auto()
+par(family = 'ComputerModern')
+# end formatting stuff
+
