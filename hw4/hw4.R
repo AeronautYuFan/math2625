8c323e9559b6a08bb70c246160669aaf496379de
AeronautYuFan <AeronautYuFan@gmail.com>
2025-03-31 11:43:07 -0400

    done

--- hw4/hw4.R
+++ hw4/hw4.R
@@ -45,3 +45,43 @@ custom_labels <- c("No Wheezing",
 
 text(y = bar_positions, x = prop_zeros / 2,
      labels = custom_labels, col = "black", cex = 1.2, font = 2)
+
+
+
+oddsratio.matched(4, 12)
+
+
+oddsratio.matched <- function(n21, n12, conf_level = 0.95) {
+  or_value <- n21 / n12
+  
+  # standard error for CI
+  se_log_or <- sqrt(1/n21 + 1/n12)
+  
+  # z score for CI
+  z_score <- qnorm(1 - (1 - conf_level) / 2)
+  
+  log_or <- log(or_value)
+  lower_log_or <- log_or - z_score * se_log_or
+  upper_log_or <- log_or + z_score * se_log_or
+  
+  # untransform ci
+  lower_or <- exp(lower_log_or)
+  upper_or <- exp(upper_log_or)
+  
+  # code for printing
+  or_value <- round(or_value, 7)
+  lower_or <- round(lower_or, 7)
+  upper_or <- round(upper_or, 7)
+  
+  output_text <- paste(
+    "Matched-pairs odds ratio (", 
+    conf_level * 100, "% confidence interval)\n", 
+    "OR: ", or_value, "\n", 
+    "Lower    Upper\n", 
+    lower_or, "    ", upper_or,
+    sep = ""
+  )
+  
+  # print
+  cat(output_text)
+}
